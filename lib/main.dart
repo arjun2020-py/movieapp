7cf1889099218e60e8f782a8ea0123c428354ac8
arjun2020-py <arjunkoodathil@gmail.com>
2024-03-27 10:29:09 +0530

    make some changes in  local storage

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,8 @@
 import 'package:flutter/material.dart';
 
-import 'screen/splash/view/splash_screen.dart';
-import 'screen/task_1/lead/screen_lead.dart';
-import 'screen/task_1/login/view/screen_login.dart';
 import 'screen/task_1/splash/screen_splash.dart';
-import 'screen/test/view/screen_leads.dart';
-import 'screen/test/view/screen_post.dart';
-import 'screen/test/view/screen_resource_list.dart';
+
+
 
 void main() {
   runApp(const MyApp());
